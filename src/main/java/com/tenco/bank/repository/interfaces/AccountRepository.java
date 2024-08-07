13c7e53a5c8a152f6e60b60e2c1d03669f861461
junsoo186 <junsoo186@gmail.com>
2024-08-07 16:47:03 +0900

    account save jsp 생성 및 컨트롤 서비스 생성

--- src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
+++ src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
@@ -2,10 +2,11 @@ package com.tenco.bank.repository.interfaces;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.tenco.bank.repository.model.Account;
-
+@Mapper
 public interface AccountRepository {
 	public int insert(Account account);
 	public int updateById(Account account);
